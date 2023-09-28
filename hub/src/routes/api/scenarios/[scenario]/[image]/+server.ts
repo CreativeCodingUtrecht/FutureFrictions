ea0bbc9c344264b5a4420020049cdb228c7d579f
Niels Janssen <niels@appliedcreative.nl>
2023-09-28 16:50:49 +0200

    Added constant to server.ts

--- hub/src/routes/api/scenarios/[scenario]/[image]/+server.ts
+++ hub/src/routes/api/scenarios/[scenario]/[image]/+server.ts
@@ -1,12 +1,13 @@
 import type { RequestHandler } from './$types';
 import { error } from '@sveltejs/kit';
 import fs from 'fs'
+import { DATAROOT } from '$lib/constants';
 
 export const GET: RequestHandler = ({ params }) => {
     const scenario = params.scenario;
     const image = params.image;
 
-    const filename = `../data/${scenario}/${image}`;
+    const filename = `${DATAROOT}/${scenario}/${image}`;
     const extension = filename.split('.').pop();
     const mime = `image/${extension}`;
 
