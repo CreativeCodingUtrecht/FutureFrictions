a9c73c4d54575eadd33974e56a01aeaf51729475
Niels Janssen <niels@appliedcreative.nl>
2023-10-02 14:29:16 +0200

    Extension change, port as number

--- hub/ecosystem.system.cjs
+++ hub/ecosystem.system.cjs
@@ -5,9 +5,9 @@ module.exports = {
           script: "build/index.js",
           watch: true,
           env: {
-            PORT: "3001",
+            PORT: 3001,
             ORIGIN: "https://futurefrictions.appliedcreative.nl",
-            DATAROOT: "../../data"
+            DATAROOT: "../data"
           }
         }
     ]
